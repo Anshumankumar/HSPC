453cb2610195215ddb675f8cb145c62bd4a19265
Anshuman <anshu266man@gmail.com>
2016-05-02 18:02:22 +0530

    Adding the kernel

--- Project/parallel.cu
+++ Project/parallel.cu
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "graph.h"
 
+__global__ void travese(int *array, int *xadj, int *adj,
+        int *current, int *Fs, int *entry)
+{
+    array[*current] = array[*current] +1;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -17,7 +23,29 @@ int main(int argc, char **argv)
         << density << "\n";
 
     graphGenerator(n,density, array,xadj,adj);
+    int * dev_array;
+    int *dev_xadj;
+    int *dev_adj;
+    cudaMalloc((void**)&dev_array, sizeof(int)*array.size());
+    cudaMalloc((void**)&dev_xadj, sizeof(int)*xadj.size());
+    cudaMalloc((void**)&dev_adj, sizeof(int)*adj.size());
 
+    cudaMemcpy( dev_array, &array[0],
+            sizeof(int)*array.size(), cudaMemcpyHostToDevice );
+
+    cudaMemcpy( dev_xadj, &xadj[0],
+            sizeof(int)*xadj.size(), cudaMemcpyHostToDevice );
+
+    cudaMemcpy( dev_adj, &adj[0],
+            sizeof(int)*adj.size(), cudaMemcpyHostToDevice );
+
+
+    cudaMemcpy(&array[0], dev_array,
+            sizeof(int)*array.size(), cudaMemcpyHostToDevice );
+    int stop =1;
+    int start = 0;
+    matrixMultiplication<<<stop-start,1>>>
+    std::cout << dev_array[n-1] <<"\n";
 #ifdef DEBUG
     print(xadj);
     print(adj);
